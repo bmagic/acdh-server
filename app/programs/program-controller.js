13ef8172ce2d0c9858c6b2ca8a288cc4826be1df
bmagic <baptiste.routhieau@gmail.com>
2017-10-16 23:10:50 +0200

    Change 201 request to 204 for newly created program

--- app/programs/program-controller.js
+++ app/programs/program-controller.js
@@ -1,130 +1,129 @@
-"use strict";
-var async = require("async");
-var programModel = require("programs/program-model.js");
-var programSchema = require("programs/program-schema");
-var HttpStatus = require('http-status-codes');
-var applicationStorage = require("core/application-storage");
-
+'use strict'
+var async = require('async')
+var programModel = require('programs/program-model.js')
+var programSchema = require('programs/program-schema')
+var HttpStatus = require('http-status-codes')
+var applicationStorage = require('core/application-storage')
 
 module.exports.getPrograms = function (req, res) {
-    var logger = applicationStorage.logger;
-    var itemNumber = 15;
-    var criteria = {};
-    if (req.query.search) {
-        //criteria = {$or:[{tags:{$regex: req.query.search, $options: "i"}}]}
-        //criteria = {$or:[{$text:{$search: req.query.search}},{tags:{$regex: req.query.search, $options: "i"}}]}
-        criteria = {$text: {$search: req.query.search}};
-    }
+  var logger = applicationStorage.logger
+  var itemNumber = 15
+  var criteria = {}
+  if (req.query.search) {
+    //criteria = {$or:[{tags:{$regex: req.query.search, $options: "i"}}]}
+    //criteria = {$or:[{$text:{$search: req.query.search}},{tags:{$regex: req.query.search, $options: "i"}}]}
+    criteria = {$text: {$search: req.query.search}}
+  }
 
-    var skip = 0;
-    if (req.query.page) {
-        var page = parseInt(req.query.page, 10);
-        if (page != NaN) {
-            if (page > 0) {
-                skip = page * itemNumber;
-            }
-        }
+  var skip = 0
+  if (req.query.page) {
+    var page = parseInt(req.query.page, 10)
+    if (page != NaN) {
+      if (page > 0) {
+        skip = page * itemNumber
+      }
     }
+  }
 
-    async.parallel({
-        programs: function (callback) {
-            programModel.find(criteria, itemNumber, skip, function (error, programs) {
-                callback(error, programs);
-            });
-        },
-        count: function (callback) {
-            programModel.count(criteria, function (error, count) {
-                callback(error, count);
-            });
-        }
-    }, function (error, results) {
-        if (error) {
-            logger.error(error);
-            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
-        } else {
-            res.header("X-count", results.count);
-            res.status(HttpStatus.OK).json(results.programs);
-        }
-    });
-};
+  async.parallel({
+    programs: function (callback) {
+      programModel.find(criteria, itemNumber, skip, function (error, programs) {
+        callback(error, programs)
+      })
+    },
+    count: function (callback) {
+      programModel.count(criteria, function (error, count) {
+        callback(error, count)
+      })
+    }
+  }, function (error, results) {
+    if (error) {
+      logger.error(error)
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR))
+    } else {
+      res.header('X-count', results.count)
+      res.status(HttpStatus.OK).json(results.programs)
+    }
+  })
+}
 
 module.exports.getTags = function (req, res) {
-    var logger = applicationStorage.logger;
-    var criteria = {};
-    if (req.query.search) {
-        criteria = {$or: [{tags: {$regex: req.query.search, $options: "i"}}]}
+  var logger = applicationStorage.logger
+  var criteria = {}
+  if (req.query.search) {
+    criteria = {$or: [{tags: {$regex: req.query.search, $options: 'i'}}]}
+  }
+  programModel.findTags(criteria, function (error, programs) {
+    if (error) {
+      logger.error(error)
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR))
+    } else {
+      res.status(HttpStatus.OK).json(programs)
     }
-    programModel.findTags(criteria, function (error, programs) {
-        if (error) {
-            logger.error(error);
-            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
-        } else {
-            res.status(HttpStatus.OK).json(programs);
-        }
-    });
-};
+  })
+}
 
 module.exports.createProgram = function (req, res) {
-    var logger = applicationStorage.logger;
-    async.waterfall([
-        function (callback) {
-            programSchema.validate(req.body, function (error, program) {
-                if (error) {
-                    logger.error(error);
-                    res.status(HttpStatus.BAD_REQUEST).send(HttpStatus.getStatusText(HttpStatus.BAD_REQUEST));
-                } else {
-                    callback(null, program);
-                }
-            });
-        },
-        function (program) {
-            programModel.insert(program, function (error) {
-                if (error) {
-                    logger.error(error);
-                    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
-                } else {
-                    res.status(HttpStatus.CREATED).send(HttpStatus.getStatusText(HttpStatus.CREATED));
-                }
-            });
+  var logger = applicationStorage.logger
+  async.waterfall([
+    function (callback) {
+      programSchema.validate(req.body, function (error, program) {
+        if (error) {
+          logger.error(error)
+          res.status(HttpStatus.BAD_REQUEST).send(HttpStatus.getStatusText(HttpStatus.BAD_REQUEST))
+        } else {
+          callback(null, program)
         }
-    ]);
-};
+      })
+    },
+    function (program) {
+      programModel.insert(program, function (error) {
+        if (error) {
+          logger.error(error)
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR))
+        } else {
+          res.status(HttpStatus.NO_CONTENT).send(HttpStatus.getStatusText(HttpStatus.NO_CONTENT))
+        }
+      })
+    }
+  ])
+}
 
 module.exports.updateProgram = function (req, res) {
-    var logger = applicationStorage.logger;
-    async.waterfall([
-        function (callback) {
-            programSchema.validate(req.body, function (error, program) {
-                if (error) {
-                    logger.error(error);
-                    res.status(HttpStatus.BAD_REQUEST).send(HttpStatus.getStatusText(HttpStatus.BAD_REQUEST));
-                } else {
-                    callback(null, program);
-                }
-            });
-        },
-        function (program) {
-            programModel.update(req.params.id, program, function (error) {
-                if (error) {
-                    logger.error(error);
-                    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
-                } else {
-                    res.status(HttpStatus.OK).send(HttpStatus.getStatusText(HttpStatus.OK));
-                }
-            });
+  var logger = applicationStorage.logger
+  async.waterfall([
+    function (callback) {
+      programSchema.validate(req.body, function (error, program) {
+        if (error) {
+          logger.error(error)
+          res.status(HttpStatus.BAD_REQUEST).send(HttpStatus.getStatusText(HttpStatus.BAD_REQUEST))
+        } else {
+          callback(null, program)
         }
-    ]);
-};
+      })
+    },
+    function (program) {
+      programModel.update(req.params.id, program, function (error) {
+        if (error) {
+          logger.error(error)
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR))
+        } else {
+          res.status(HttpStatus.OK).send(HttpStatus.getStatusText(HttpStatus.OK))
+        }
+      })
+    }
+  ])
+}
 
 module.exports.deleteProgram = function (req, res) {
-    var logger = applicationStorage.logger;
-    programModel.delete(req.params.id, function (error) {
-        if (error) {
-            logger.error(error);
-            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
-        } else {
-            res.status(HttpStatus.NO_CONTENT).send(HttpStatus.getStatusText(HttpStatus.NO_CONTENT));
-        }
-    });
+  var logger = applicationStorage.logger
+  programModel.delete(req.params.id, function (error) {
+    if (error) {
+      logger.error(error)
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(HttpStatus.getStatusText(HttpStatus.INTERNAL_SERVER_ERROR))
+    } else {
+      res.status(HttpStatus.NO_CONTENT).send(HttpStatus.getStatusText(HttpStatus.NO_CONTENT))
+    }
+  })
 
 }
